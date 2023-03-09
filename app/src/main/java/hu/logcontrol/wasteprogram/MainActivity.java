9aa60d0c936a64e3372cf08e3f23887451c4f3a8
galmihaly <galmihaly118@gmail.com>
2023-03-09 23:20:51 +0100

    ppp

--- app/src/main/java/hu/logcontrol/wasteprogram/MainActivity.java
+++ app/src/main/java/hu/logcontrol/wasteprogram/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements IMainView {
 
     @Override
     public void exitApplication() {
-        finishAffinity();
+        MainActivity.this.finish();
         System.exit(0);
     }
 }
